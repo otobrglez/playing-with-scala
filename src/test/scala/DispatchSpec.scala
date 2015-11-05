eadbd840ab5fc25fd7a92b8efc3483b37a460fcf
Oto Brglez <otobrglez@gmail.com>
2015-11-05 20:10:12 +0100

    Daily update.

--- src/test/scala/DispatchSpec.scala
+++ src/test/scala/DispatchSpec.scala
@@ -8,14 +8,9 @@ class DispatchSpec extends FlatSpec {
 
   it should "do dispatch" in {
     val response: Future[String] = Http.configure(_ setFollowRedirects true)(url("http://icanhazptr.com/") OK as.String)
-
     response onComplete {
-      case Success(content) =>
-        assert(content.length > 3)
-
-      case Failure(content) =>
-        println(content.getMessage)
-
+      case Success(content) => assert(content.length > 3)
+      case Failure(content) => println(content.getMessage)
     }
   }
 }
